d595ba623e9126477a966bd7f609e295edcfe5af
Patryk Laba <patryk.laba@icloud.com>
2017-05-19 10:59:09 +0200

    Cog icon added

--- src/Tree/TreeData.php
+++ src/Tree/TreeData.php
@@ -40,10 +40,10 @@ class TreeData {
 			$html .= "<ul class='tree'>";
 			foreach ($category['parent_cats'][$parent] as $cat_id) {
 				if (!isset($category['parent_cats'][$cat_id])) {
-					$html .= "<li><label>". $category['categories'][$cat_id]['label'] . "<a class='btn btn-xs btn-primary' href='edit.php?id=" .$category['categories'][$cat_id]['id']."'> Edit  </a> <a class='btn btn-xs btn-danger' href='delete.php?id=" .$category['categories'][$cat_id]['id']."'> Delete  </a><a class='btn btn-xs btn-info' href='details.php?id=" .$category['categories'][$cat_id]['id']."'> Details  </a></label> <input type='checkbox'/></li>";
+					$html .= "<li><label>". $category['categories'][$cat_id]['label'] . "<a class='btn btn-xs btn-default' href='details.php?id=" .$category['categories'][$cat_id]['id']."'> <span class='glyphicon glyphicon-cog' aria-hidden='true'></span></a></label> <input type='checkbox'/></li>";
 				}
 				if (isset($category['parent_cats'][$cat_id])) {
-					$html .= "<li><label>". $category['categories'][$cat_id]['label'] . "<a class='btn btn-xs btn-primary' href='edit.php?id=" .$category['categories'][$cat_id]['id']."'> Edit  </a> <a class='btn btn-xs btn-danger' href='delete.php?id=" .$category['categories'][$cat_id]['id']."'> Delete  </a><a class='btn btn-xs btn-info' href='details.php?id=" .$category['categories'][$cat_id]['id']."'> Details  </a></label> <input type='checkbox'/>";
+					$html .= "<li><label>". $category['categories'][$cat_id]['label'] . "<a class='btn btn-xs btn-default' href='details.php?id=" .$category['categories'][$cat_id]['id']."'><span class='glyphicon glyphicon-cog' aria-hidden='true'></span></a></label> <input type='checkbox'/>";
 					$html .= $this->buildTreeView ($cat_id, $category);
 					$html .= "</li>";
 				}
