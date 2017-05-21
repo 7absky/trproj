5e8e79f76285056e23223d091ad10409df41247b
Patryk Laba <patryk.laba@icloud.com>
2017-05-21 17:34:16 +0200

    Edit feature updated

--- public/edit.php
+++ public/edit.php
@@ -2,20 +2,23 @@
 	require '../src/Tree/Autoloader.php';
 
 	if (isset($_POST['id']) && !empty($_POST['id'])) {
-		$data = new \Tree\TreeData();
-		if ($data->update($_POST)) {
-			header ("Location: /ideotree/public");
-			exit;
-		} else {
-			echo "Wystąpił błąd";
-			exit;
-		}
+        $data = new \Tree\TreeData();
+        if ($data->update($_POST)) {
+            header("Location: /ideotree/public");
+            exit;
+        } else {
+            echo "Wystąpił błąd";
+            exit;
+        }
 	}
+
 
 	if (!isset($_GET['id']) || empty($_GET['id'])) {
 		echo "Nie podałeś żadnego ID.";
 		exit;
 	}
+
+
 	$data = new \Tree\TreeData();
 	$node = $data->getNode($_GET['id']);
     $labels = $data->getLabels();
